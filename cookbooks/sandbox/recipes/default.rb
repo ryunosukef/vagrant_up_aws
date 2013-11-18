63cfaee0ad9e54b719ca145c3bf5830d29fc557b
R.Fujimoto <ryunosuke.fujimoto@greenitsystems.co.jp>
2013-11-18 13:38:05 +0900

    add cookbooks

---
+++ cookbooks/sandbox/recipes/default.rb
@@ -0,0 +1,11 @@
+#
+# Cookbook Name:: sandbox
+# Recipe:: default
+#
+# Copyright 2013, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+template "/tmp/vagrant_test.txt" do
+  mode 0644
+end
